8b8f2327fc0bc06395e70149cc7d8045691417fb
EngiBeerHub <engibeer0422@gmail.com>
2026-08-23 20:44:33 +0900

    Initial Commit

---
+++ UsedWellTests/UsedWellTests.swift
@@ -0,0 +1,19 @@
+//
+//  UsedWellTests.swift
+//  UsedWellTests
+//
+//  Created by RyosukeSeki on 2026/08/23.
+//
+
+import Testing
+@testable import UsedWell
+
+struct UsedWellTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
